a0bd50d40fae13b42ab67de15de12b069c1c1220
Luke Roberts <email@luke-roberts.co.uk>
2016-01-26 17:16:39 -0800

    clone into known volume dir

--- clone.sh
+++ clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 echo "cloning git repo"
-git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git -b ${BASE_BRANCH} code
+git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git -b ${BASE_BRANCH} /pushbit/code
 
 $1
 
