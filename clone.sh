d1011d64e274641839b6dc36fe281735c5941448
Luke Roberts <email@luke-roberts.co.uk>
2016-02-05 13:22:09 -0800

    clone.sh

--- clone.sh
+++ clone.sh
@@ -1,7 +1,16 @@
-#!/bin/bash 
+#!/bin/bash -e
 
-echo "cloning git repo"
-git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git -b ${BASE_BRANCH} /pushbit/code
+sleep 3
+
+echo "cloning git repo from:"
+echo https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git /pushbit/code
+
+git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git /pushbit/code
+
+echo "entering git repo: /pushbit/code"
+cd /pushbit/code
+
+echo "checking out branch/commit: ${BASE_BRANCH}"
+git checkout ${BASE_BRANCH}
 
 $1
-
--- copy.sh
+++ copy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
-cp /pushbit/code ~/code
+sleep 2
+
+echo "Copying code from volume"
+cp /pushbit/code /code
 
 $1
